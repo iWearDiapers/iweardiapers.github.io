92c22840ef18bb5d00160f4fb0d6d4eaa2f91fcd
system_user <tamir.arb@gmail.com>
2018-01-16 18:17:33 -0600

    this worked in codepen - made the output of marked() come after react element property "dangerouslySetInnerHTML"

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ class App extends React.Component {
 		}
 	}
 	
+	renderMarkdown(str) {
+		return marked(str);
+	}
+	
 	render() {
 		return (
 			<div>
 				<div className = ''/>
 				<textarea />
-				{marked('# Marked in browser\n\nRendered by **marked**.');}
+				<div dangerouslySetInnerHTML={{__html: this.renderMarkdown('# Marked in browser\n\nRendered by **marked**.')}}></div>
 				<h1>THIS IS AFTER MARKDOWN</h1>
 			</div>
 		);
